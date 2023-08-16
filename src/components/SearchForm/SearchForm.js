35f7101bfd784af251fd11b714c5602b0481b74b
Алексей <electrifyx@gmail.com>
2023-08-16 14:04:54 +0300

    fix bags

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,22 +1,18 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Input, Space } from 'antd';
+import { Input } from 'antd';
 
 const { Search } = Input;
-const onSearch = value => console.log(value);
 
 export default function SearchForm() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  function onFormSubmit(e) {
-    e.preventDefault();
-    const value = e.target.elements.query.value;
-
+  const onSearch = value => {
     navigate({
       ...location,
       search: `query=${value}`,
     });
-  }
+  };
 
   return (
     <div>
@@ -26,14 +22,11 @@ export default function SearchForm() {
           allowClear
           enterButton="Search"
           size="medium"
-          onSearch={onFormSubmit}
+          onSearch={onSearch}
           style={{
             width: '30%',
           }}
         />
-
-        {/* <input type="text" name="query" />
-        <button type="submit">Search</button> */}
       </form>
     </div>
   );
